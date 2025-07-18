81f68d0cafd5f0b27f2a094e32184e9fa9ddabc9
Escobar_075 <miguelpmescobar@gmail.com>
2025-07-18 14:38:06 -0300

    Update quadrilatero.c

--- figuras/quadrilatero.c
+++ figuras/quadrilatero.c
@@ -4,11 +4,11 @@
 #include "quadrilatero.h"
 
 void calcularAreaQuadrilatero() {
-    double modo, area, a, b, h;
+    double modo, area, lado_1, lado_2, altura, diagonal_1, diagonal_2;
 
-    printf("\n VocÍ escolheu o quadril·tero. Qual tipo È:\n 1 - Quadrado\n 2 - Ret‚ngulo\n 3 - Losango\n 4 - TrapÈzio\n 5 - Paralelograma\n   ");
+    printf("\n Voc√™ escolheu o quadril√°tero. Qual tipo √©:\n 1 - Quadrado\n 2 - Ret√¢ngulo\n 3 - Losango\n 4 - Trap√©zio\n 5 - Paralelograma\n   ");
     if (scanf("%lf", &modo) != 1) {
-        printf("\nEntrada inv·lida para o modo. Tente novamente.\n");
+        printf("\nEntrada inv√°lida para o modo. Tente novamente.\n");
         limparEntrada();
         return;
     }
@@ -16,43 +16,43 @@ void calcularAreaQuadrilatero() {
     switch ((int)modo) {
         case 1:
             printf("\n Digite o lado do quadrado: ");
-            if (scanf("%lf", &a) != 1) break;
-            area = pow(a, 2);
-            printf("\n A ·rea do quadrado È: %.5lf\n", area);
+            if (scanf("%lf", &lado_1) != 1) break;
+            area = pow(lado_1, 2);
+            printf("\n A √°rea do quadrado √©: %.5lf\n", area);
             break;
 
         case 2:
-            printf("\n Digite a base do ret‚ngulo: ");
-            if (scanf("%lf", &a) != 1) break;
-            printf("\n Digite a altura do ret‚ngulo: ");
-            if (scanf("%lf", &b) != 1) break;
-            area = a * b;
-            printf("\n A ·rea do ret‚ngulo È: %.5lf\n", area);
+            printf("\n Digite a base do ret√¢ngulo: ");
+            if (scanf("%lf", &lado_1) != 1) break;
+            printf("\n Digite a altura do ret√¢ngulo: ");
+            if (scanf("%lf", &lado_2) != 1) break;
+            area = lado_1 * lado_2;
+            printf("\n A √°rea do ret√¢ngulo √©: %.5lf\n", area);
             break;
 
         case 3:
             printf("\n Digite a diagonal maior do losango: ");
-            if (scanf("%lf", &a) != 1) break;
+            if (scanf("%lf", &diagonal_1) != 1) break;
             printf("\n Digite a diagonal menor do losango: ");
-            if (scanf("%lf", &b) != 1) break;
-            if (a > b) {
-                area = (a * b) / 2;
-                printf("\n A ·rea do losango È: %.5lf\n", area);
+            if (scanf("%lf", &diagonal_2) != 1) break;
+            if (diagonal_1 > diagonal_2) {
+                area = (diagonal_1 * diagonal_2) / 2;
+                printf("\n A √°rea do losango √©: %.5lf\n", area);
             } else {
                 printf("\n A diagonal maior deve ser realmente maior. TENTE NOVAMENTE!\n");
             }
             break;
 
         case 4:
-            printf("\n Digite a base maior do trapÈzio: ");
-            if (scanf("%lf", &a) != 1) break;
-            printf("\n Digite a base menor do trapÈzio: ");
-            if (scanf("%lf", &b) != 1) break;
-            printf("\n Digite a altura do trapÈzio: ");
-            if (scanf("%lf", &h) != 1) break;
-            if (a > b) {
-                area = ((a + b) * h) / 2;
-                printf("\n A ·rea do trapÈzio È: %.5lf\n", area);
+            printf("\n Digite a base maior do trap√©zio: ");
+            if (scanf("%lf", &lado_1) != 1) break;
+            printf("\n Digite a base menor do trap√©zio: ");
+            if (scanf("%lf", &lado_2) != 1) break;
+            printf("\n Digite a altura do trap√©zio: ");
+            if (scanf("%lf", &altura) != 1) break;
+            if (lado_1 > lado_2) {
+                area = ((lado_1 + lado_2) * altura) / 2;
+                printf("\n A √°rea do trap√©zio √©: %.5lf\n", area);
             } else {
                 printf("\n A base maior deve ser realmente maior. TENTE NOVAMENTE!\n");
             }
@@ -60,15 +60,15 @@ void calcularAreaQuadrilatero() {
 
         case 5:
             printf("\n Digite a base do paralelograma: ");
-            if (scanf("%lf", &a) != 1) break;
+            if (scanf("%lf", &lado_1) != 1) break;
             printf("\n Digite a altura do paralelograma: ");
-            if (scanf("%lf", &h) != 1) break;
-            area = a * h;
-            printf("\n A ·rea do paralelograma È: %.5lf\n", area);
+            if (scanf("%lf", &altura) != 1) break;
+            area = lado_1 * altura;
+            printf("\n A √°rea do paralelograma √©: %.5lf\n", area);
             break;
 
         default:
-            printf("\nModo inv·lido. TENTE NOVAMENTE!\n");
+            printf("\nModo inv√°lido. TENTE NOVAMENTE!\n");
     }
     limparEntrada();
 }
