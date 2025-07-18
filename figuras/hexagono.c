7b574ffa9e08e0df0728cfb98bd5793ecd24382e
Escobar_075 <miguelpmescobar@gmail.com>
2025-07-18 14:39:34 -0300

    Update hexagono.c

--- figuras/hexagono.c
+++ figuras/hexagono.c
@@ -4,15 +4,15 @@
 #include "hexagono.h"
 
 void calcularAreaHexagono() {
-    double a, area;
-    printf("\n VocÍ escolheu o hex·gono regular");
+    double lado_1, area;
+    printf("\n Voc√™ escolheu o hex√°gono regular");
     printf("\n Digite o valor do lado: ");
-    if (scanf("%lf", &a) != 1) {
-        printf("\nEntrada inv·lida. TENTE NOVAMENTE!\n");
+    if (scanf("%lf", &lado_1) != 1) {
+        printf("\nEntrada inv√°lida. TENTE NOVAMENTE!\n");
         limparEntrada();
         return;
     }
-    area = 3 * (pow(a, 2) * sqrt(3)) / 2;
-    printf("\n A ·rea do hex·gono È: %.5lf\n", area);
+    area = 3 * (pow(lado_1, 2) * sqrt(3)) / 2;
+    printf("\n A √°rea do hex√°gono √©: %.5lf\n", area);
     limparEntrada();
 }
