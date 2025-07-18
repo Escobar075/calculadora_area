252b9096b133dfc9f5b0f52107b5226a4ff8fae9
Escobar_075 <miguelpmescobar@gmail.com>
2025-07-18 14:42:45 -0300

    Update circunferencia.c

--- figuras/circunferencia.c
+++ figuras/circunferencia.c
@@ -4,11 +4,11 @@
 #include "circunferencia.h"
 
 void calcularAreaCircunferencia() {
-    double r, R, b, modo, angulo, area;
+    double raio_menor, raio_maior, unidade_angular, modo, angulo, area;
 
-    printf("\n VocÍ escolheu a circunferÍncia. Escolha uma opÁ„o:\n 1 - ¡rea\n 2 - Setor circular\n 3 - CÌrculo anular\n   ");
+    printf("\n Voc√™ escolheu a circunfer√™ncia. Escolha uma op√ß√£o:\n 1 - √Årea\n 2 - Setor circular\n 3 - C√≠rculo anular\n   ");
     if (scanf("%lf", &modo) != 1) {
-        printf("\nEntrada inv·lida. TENTE NOVAMENTE!\n");
+        printf("\nEntrada inv√°lida. TENTE NOVAMENTE!\n");
         limparEntrada();
         return;
     }
@@ -16,47 +16,47 @@ void calcularAreaCircunferencia() {
     switch ((int)modo) {
         case 1:
             printf("\n Digite o valor do raio: ");
-            if (scanf("%lf", &r) != 1) break;
-            area = pow(r, 2) * M_PI;
-            printf("\n A ·rea da circunferÍncia È: %.5lf\n", area);
+            if (scanf("%lf", &raio_menor) != 1) break;
+            area = pow(raio_menor, 2) * M_PI;
+            printf("\n A √°rea da circunfer√™ncia √©: %.5lf\n", area);
             break;
 
         case 2:
             printf("\n Digite o valor do raio: ");
-            if (scanf("%lf", &r) != 1) break;
-            printf("\n Escolha a unidade do ‚ngulo:\n 1 - Grau\n 2 - Radiano\n 3 - Grado\n   ");
-            if (scanf("%lf", &b) != 1) break;
-            printf("\n Digite o valor do ‚ngulo: ");
+            if (scanf("%lf", &raio_menor) != 1) break;
+            printf("\n Escolha a unidade do √¢ngulo:\n 1 - Grau\n 2 - Radiano\n 3 - Grado\n   ");
+            if (scanf("%lf", &unidade_angular) != 1) break;
+            printf("\n Digite o valor do √¢ngulo: ");
             if (scanf("%lf", &angulo) != 1) break;
 
-            if (b == 1) {
-                area = angulo * pow(r, 2) * M_PI / 360;
-            } else if (b == 2) {
-                area = angulo * M_PI * pow(r, 2) / 2;
-            } else if (b == 3) {
-                area = angulo * M_PI * pow(r, 2) / 400;
+            if (unidade_angular == 1) {
+                area = angulo * pow(raio_menor, 2) * M_PI / 360;
+            } else if (unidade_angular == 2) {
+                area = angulo * M_PI * pow(raio_menor, 2) / 2;
+            } else if (unidade_angular == 3) {
+                area = angulo * M_PI * pow(raio_menor, 2) / 400;
             } else {
-                printf("\n Unid. de ‚ngulo inv·lida.\n");
+                printf("\n Unid. de √¢ngulo inv√°lida.\n");
                 return;
             }
-            printf("\n A ·rea do setor circular È: %.5lf\n", area);
+            printf("\n A √°rea do setor circular √©: %.5lf\n", area);
             break;
 
         case 3:
             printf("\n Digite o raio maior: ");
-            if (scanf("%lf", &R) != 1) break;
+            if (scanf("%lf", &raio_maior) != 1) break;
             printf("\n Digite o raio menor: ");
-            if (scanf("%lf", &r) != 1) break;
-            if (R > r) {
-                area = M_PI * (pow(R, 2) - pow(r, 2));
-                printf("\n A ·rea do cÌrculo anular È: %.5lf\n", area);
+            if (scanf("%lf", &raio_menor) != 1) break;
+            if (raio_maior > raio_menor) {
+                area = M_PI * (pow(raio_maior, 2) - pow(raio_menor, 2));
+                printf("\n A √°rea do c√≠rculo anular √©: %.5lf\n", area);
             } else {
                 printf("\n O raio maior deve ser maior que o menor.\n");
             }
             break;
 
         default:
-            printf("\nModo inv·lido. TENTE NOVAMENTE!\n");
+            printf("\nModo inv√°lido. TENTE NOVAMENTE!\n");
     }
     limparEntrada();
 }
