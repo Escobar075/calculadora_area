ada9428b3acf366c7b784015d8f60efd8c6ec367
Escobar_075 <miguelpmescobar@gmail.com>
2025-07-22 15:35:18 -0300

    Update pentagono.c

--- figuras/pentagono.c
+++ figuras/pentagono.c
@@ -4,15 +4,15 @@
 #include "pentagono.h"
 
 void calcularAreaPentagono() {
-    double lado_1, area;
-    printf("\n Vocę escolheu o pentágono regular");
+    double lado, area;
+    printf("\n Voce escolheu o pentágono regular");
     printf("\n Digite o valor do lado: ");
-    if (scanf("%lf", &lado_1) != 1) {
+    if (scanf("%lf", &lado) != 1) {
         printf("\nEntrada inválida. TENTE NOVAMENTE!\n");
         limparEntrada();
         return;
     }
-    area = (5 * pow(lado_1, 2)) / (4 * tan(M_PI / 5));
+    area = (5 * pow(lado, 2)) / (4 * tan(M_PI / 5));
     printf("\n A área do pentágono é: %.5lf\n", area);
     limparEntrada();
 }
