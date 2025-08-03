a230ff55ed6499122145b64c25dd241cbd3691d1
Escobar_075 <miguelpmescobar@gmail.com>
2025-08-03 16:17:17 -0300

    Update triangulo.c

--- figuras/triangulo.c
+++ figuras/triangulo.c
@@ -3,53 +3,84 @@
 #include "../utils/entrada.h"
 #include "triangulo.h"
 
-void calcularAreaTriangulo() {
-    int modo;
-    double lado_1, lado_2, lado_3, altura, semiperimetro, angulo, radiano, area;
+void calcularAreaTriangulo_BaseAltura() {
+	double base, altura, area;
+	
+	if (!lerDouble("\n Digite a base do triângulo: ", &base) || !lerDouble("\n Digite a altura do triângulo: ", &altura)) return;
+	area = (base * altura) / 2;	
+	printf("\nA área do triângulo é: %.5lf\n", area);
+}
 
-    printf("\nVocê escolheu o triângulo. Escolha o modo de cálculo:\n");
-    printf(" 1 - Base e altura\n 2 - Três lados\n 3 - Dois lados e ângulo\n 4 - Equilátero\n 5 - Retângulo\n => ");
+void calcularAreaTriangulo_TodosLados() {
+	double lado_1, lado_2, lado_3, semiperimetro, area;
+	
+	if (!lerDouble("\n Digite o lado 1 do triângulo: ", &lado_1) || !lerDouble("\n Digite o lado 2 do triângulo: ", &lado_2) || !lerDouble("\n Digite o lado 3 do triângulo: ", &lado_3)) return;
+  	if (!(lado_1 + lado_2 > lado_3 && lado_1 + lado_3 > lado_2 && lado_2 + lado_3 > lado_1)) {
+		printf("\n Lados inválidos.\n");
+		return;
+	} else {
+		semiperimetro = (lado_1 + lado_2 + lado_3) / 2;
+		area = sqrt(semiperimetro * (semiperimetro - lado_1) * (semiperimetro - lado_2) * (semiperimetro - lado_3));
+	return;
+	}
+	printf("\nA área do triângulo é: %.5lf\n", area);
+}
 
-    if (scanf("%d", &modo) != 1) {
-        printf("\nEntrada inválida.\n");
-        limparEntrada();
-        return;
-    }
+void calcularAreaTriangulo_DoisLadosAngulo() {
+	int unidade;
+	double lado_1, lado_2, angulo, area;
 
+	if (!lerDouble("\n Digite o lado 1 do triângulo: ", &lado_1) ||
+	    !lerDouble("\n Digite o lado 2 do triângulo: ", &lado_2) ||
+	    !lerInt("\n Escolha a unidade do ângulo:\n 1 - Grau\n 2 - Radiano\n 3 - Grado\n => ", &unidade)) return;
+
+	if (!lerDouble("\n Digite o ângulo: ", &angulo)) return;
+
+	switch (unidade) {
+		case 1: angulo = angulo * M_PI / 180.0; 
+			break;
+		case 2: 
+			break; 
+		case 3: angulo = angulo * M_PI / 200.0; break;
+		default:
+			printf("\n Unidade inválida.\n");
+			return;
+	}
+
+	area = lado_1 * lado_2 * sin(angulo) / 2.0;
+	printf("\n A área do triângulo é: %.5lf\n", area);
+}
+
+void calcularAreaTriangulo_Equilatero() {
+	double lado, area;
+	
+	if (!lerDouble("\n Digite o lado do triângulo equilatero: ", &lado)) return;
+	area = (pow(lado, 2) * sqrt(3)) / 4;
+    printf("\nA área do triângulo é: %.5lf\n", area);
+}
+
+void menuCalculoAreaTriangulo() {
+	int modo;
+	
+	if(!lerInt("\n Escolha o modo de cálculo do triângulo: \n 1 - Base e altura\n 2 - Três lados\n 3 - Dois lados e ângulo\n 4 - Equilátero\n => ", &modo)) return;
+	
     switch (modo) {
         case 1:
-            if (!lerDouble("\nBase: ", &lado_1) || !lerDouble("Altura: ", &altura)) return;
-            area = (lado_1 * altura) / 2;
+			calcularAreaTriangulo_BaseAltura();
             break;
         case 2:
-            if (!lerDouble("\nLado 1: ", &lado_1) || !lerDouble("Lado 2: ", &lado_2) || !lerDouble("Lado 3: ", &lado_3)) return;
-            if (lado_1 + lado_2 > lado_3 && lado_1 + lado_3 > lado_2 && lado_2 + lado_3 > lado_1) {
-                semiperimetro = (lado_1 + lado_2 + lado_3) / 2;
-                area = sqrt(semiperimetro * (semiperimetro - lado_1) * (semiperimetro - lado_2) * (semiperimetro - lado_3));
-            } else {
-                printf("\nLados inválidos.\n");
-                return;
-            }
+            calcularAreaTriangulo_TodosLados();
             break;
         case 3:
-            if (!lerDouble("\nLado 1: ", &lado_1) || !lerDouble("Lado 2: ", &lado_2) || !lerDouble("Ângulo (graus): ", &angulo)) return;
-            radiano = angulo * M_PI / 180;
-            area = lado_1 * lado_2 * sin(radiano) / 2;
+			calcularAreaTriangulo_DoisLadosAngulo();
             break;
         case 4:
-            if (!lerDouble("\nLado: ", &lado_1)) return;
-            area = (pow(lado_1, 2) * sqrt(3)) / 4;
-            break;
-        case 5:
-            if (!lerDouble("\nBase: ", &lado_1) || !lerDouble("Altura: ", &altura)) return;
-            area = (lado_1 * altura) / 2;
+			calcularAreaTriangulo_Equilatero();
             break;
         default:
             printf("\nModo inválido.\n");
             return;
     }
 
-    printf("\nA área do triângulo é: %.5lf\n", area);
     limparEntrada();
 }
-
