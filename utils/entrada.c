212393d79abd6f0839cde816b631ed753a88af93
Escobar_075 <miguelpmescobar@gmail.com>
2025-08-03 16:10:56 -0300

    Update entrada.c

--- utils/entrada.c
+++ utils/entrada.c
@@ -8,16 +8,30 @@ void limparEntrada() {
 
 void limparTela() {
 #ifdef _WIN32
-    //system("cls");
-    printf("\e[1;1H\e[2J");
+    system("cls");
 #else
+	if (getenv("TERM") != NULL && strcmp(getenv("TERM"), "dumb") != 0) {
+        printf("\e[1;1H\e[2J"); 
+	} else {
     system("clear");
+	}
 #endif
+	fflush(stdout);
 }
 
 int lerDouble(const char* mensagem, double* valor) {
     printf("%s", mensagem);
     if (scanf("%lf", valor) != 1) {
+        limparEntrada();
+        printf("\nEntrada inválida. Tente novamente.\n");
+        return 0;
+    }
+    return 1;
+}
+
+int lerInt(const char* mensagem, int* valor) {
+    printf("%s", mensagem);
+    if (scanf("%d", valor) != 1) {
         limparEntrada();
         printf("\nEntrada inválida. Tente novamente.\n");
         return 0;
