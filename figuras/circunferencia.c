f8ff401e77f9f1542bfc616a698c92b833022413
Escobar_075 <miguelpmescobar@gmail.com>
2025-08-03 16:15:00 -0300

    Update circunferencia.c

--- figuras/circunferencia.c
+++ figuras/circunferencia.c
@@ -3,56 +3,66 @@
 #include "../utils/entrada.h"
 #include "circunferencia.h"
 
-void calcularAreaCircunferencia() {
-    double raio_menor, raio_maior, unidade_angular, modo, angulo, area;
+void calcularAreaCircunferencia_AreaTotal(){
+	double raio, area;
+	
+	if(!lerDouble("\n Digite o raio da circunferência: ", &raio)) return;
+	area = pow(raio,2) * M_PI;
+	printf("\n A área da circunferência é: %.5lf\n", area);
+}
 
-    printf("\n Você escolheu a circunferência. Escolha uma opção:\n 1 - Área\n 2 - Setor circular\n 3 - Círculo anular\n   ");
-    if (scanf("%lf", &modo) != 1) {
-        printf("\nEntrada inválida. TENTE NOVAMENTE!\n");
-        limparEntrada();
-        return;
+void calcularAreaCircunferencia_SetorCircular(){
+    int unidade;
+    double raio, angulo;
+    if (!lerDouble("\n Digite o raio do setor circular: ", &raio)) return;
+    if (!lerInt("\n Escolha a unidade do ângulo:\n 1 - Grau\n 2 - Radiano\n 3 - Grado\n => ", &unidade)) return;
+    if (!lerDouble("\n Digite o ângulo: ", &angulo)) return;
+
+    double area = 0;
+    switch (unidade) {
+        case 1: area = angulo * PI * raio * raio / 360;
+			break;
+        case 2: area = angulo * raio * raio / 2; 
+			break;
+        case 3: area = angulo * PI * raio * raio / 400; 
+			break;
+        default:
+            printf("\n Unidade inválida.\n");
+            return;
     }
+    printf("\n A área do setor circular é: %.5lf\n", area);
+}
+	
+}
+
+void calcularAreaCircunferencia_CirculoAnular(){
+	double raio_maior, raio_menor, area;
+	
+	if(!lerDouble("\n Digite o raio maior da circunferência: ", &raio_maior) || !lerDouble("\n Digite o raio menor da circunferência: ", &raio_menor)) return;
+    if (raio_maior > raio_menor) {
+    	area = M_PI * (pow(raio_maior, 2) - pow(raio_menor, 2));
+        printf("\n A área do círculo anular é: %.5lf\n", area);
+	} else {
+        printf("\n O raio maior deve ser maior que o menor.\n");
+	}
+}
+
+void menuCalculoAreaCircunferencia() {
+	int modo;
+
+	if(!lerInt("\n Escolha uma opção sobre circunferência:\n 1 - Área\n 2 - Setor circular\n 3 - Círculo anular\n =>", &modo)) return;
 
     switch ((int)modo) {
         case 1:
-            printf("\n Digite o valor do raio: ");
-            if (scanf("%lf", &raio_menor) != 1) break;
-            area = pow(raio_menor, 2) * M_PI;
-            printf("\n A área da circunferência é: %.5lf\n", area);
+            calcularAreaCircunferencia_AreaTotal();
             break;
 
         case 2:
-            printf("\n Digite o valor do raio: ");
-            if (scanf("%lf", &raio_menor) != 1) break;
-            printf("\n Escolha a unidade do ângulo:\n 1 - Grau\n 2 - Radiano\n 3 - Grado\n   ");
-            if (scanf("%lf", &unidade_angular) != 1) break;
-            printf("\n Digite o valor do ângulo: ");
-            if (scanf("%lf", &angulo) != 1) break;
-
-            if (unidade_angular == 1) {
-                area = angulo * pow(raio_menor, 2) * M_PI / 360;
-            } else if (unidade_angular == 2) {
-                area = angulo * M_PI * pow(raio_menor, 2) / 2;
-            } else if (unidade_angular == 3) {
-                area = angulo * M_PI * pow(raio_menor, 2) / 400;
-            } else {
-                printf("\n Unid. de ângulo inválida.\n");
-                return;
-            }
-            printf("\n A área do setor circular é: %.5lf\n", area);
+            calcularAreaCircunferencia_SetorCircular();
             break;
 
         case 3:
-            printf("\n Digite o raio maior: ");
-            if (scanf("%lf", &raio_maior) != 1) break;
-            printf("\n Digite o raio menor: ");
-            if (scanf("%lf", &raio_menor) != 1) break;
-            if (raio_maior > raio_menor) {
-                area = M_PI * (pow(raio_maior, 2) - pow(raio_menor, 2));
-                printf("\n A área do círculo anular é: %.5lf\n", area);
-            } else {
-                printf("\n O raio maior deve ser maior que o menor.\n");
-            }
+            calcularAreaCircunferencia_CirculoAnular();
             break;
 
         default:
